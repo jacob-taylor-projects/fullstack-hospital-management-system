7b6ede9e66401e92eee271b1eb8748fd5af8485b
Jacob Taylor <jtlikesps4@icloud.com>
2024-08-07 18:04:47 -0500

    login endpoints

--- hospital-Management-System-Backend/hospital-Management-System-Backend/src/main/java/com/project/hospital_Management_System_Backend/controllers/EmployeesController.java
+++ hospital-Management-System-Backend/hospital-Management-System-Backend/src/main/java/com/project/hospital_Management_System_Backend/controllers/EmployeesController.java
@@ -1,7 +1,11 @@
 package com.project.hospital_Management_System_Backend.controllers;
 
+import com.project.hospital_Management_System_Backend.dtos.CredentialsDto;
+import com.project.hospital_Management_System_Backend.dtos.FullEmployeeDto;
 import com.project.hospital_Management_System_Backend.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class EmployeesController {
     private final EmployeeService employeeService;
+
+    @PostMapping("/login")
+    public FullEmployeeDto login(@RequestBody CredentialsDto credentialsDTO){
+        return employeeService.login(credentialsDTO);
+    }
 }
--- hospital-Management-System-Backend/hospital-Management-System-Backend/src/main/java/com/project/hospital_Management_System_Backend/controllers/PatientsController.java
+++ hospital-Management-System-Backend/hospital-Management-System-Backend/src/main/java/com/project/hospital_Management_System_Backend/controllers/PatientsController.java
@@ -1,6 +1,11 @@
 package com.project.hospital_Management_System_Backend.controllers;
 
+import com.project.hospital_Management_System_Backend.dtos.CredentialsDto;
+import com.project.hospital_Management_System_Backend.dtos.FullPatientDto;
+import com.project.hospital_Management_System_Backend.services.PatientService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/patients")
 @RequiredArgsConstructor
 public class PatientsController {
+    private final PatientService patientService;
+    @PostMapping("/login")
+    public FullPatientDto login(@RequestBody CredentialsDto credentialsDTO){
+        return patientService.login(credentialsDTO);
+    }
 }
